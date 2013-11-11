2041e1ad987f8d86b4e53fcfe58ce18a721f21bb
AKAfreaky <frogman.jo@gmail.com>
2013-11-11 13:38:56 +0000

    Support rectangular matrix and split into functions
    Use arrayX/arrayY in most functions instead of just arraySize.
    Moved the averaging and the loop to own functions

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 #include <pthread.h>
 #include "arrayHelpers.h"
 
-int checkDiff( int** oldArray, int** newArray, int arraySize, int precision)
+int checkDiff( int** oldArray, int** newArray, int arrayX, int arrayY, int precision)
 {
 	int i,j, retVal = 1; // assume we will pass
 
-	for(i = 1; i < arraySize - 1; i++)
+	for(i = 1; i < arrayX - 1; i++)
 	{
-		for(j = 1; j < arraySize - 1; j++)
+		for(j = 1; j < arrayY - 1; j++)
 		{
 			int oldVal = oldArray[i][j],
 				newVal = newArray[i][j];
@@ -27,13 +27,75 @@ int checkDiff( int** oldArray, int** newArray, int arraySize, int precision)
 }
 
 
+void averageFour( int** inArray, int** outArray, int arrayX, int arrayY)
+{
+	int i, j;
+	//from pos 1 to arraySize-2 as edges are fixed
+	for(i = 1; i < arrayX - 1; i++)
+	{
+		for(j = 1; j < arrayY - 1; j++)
+		{
+			int n,s,e,w;
+			n	=	inArray[i-1][j];
+			s	=	inArray[i+1][j];
+			e	=	inArray[i][j+1];
+			w	=	inArray[i][j-1];
+
+			outArray[i][j] = (n + s + e + w) / 4;
+
+		}
+	}
+
+}
+
+void threadAverage( int** inArray, int** outArray, int arrayX, int arrayY, int precision)
+{
+	int i, diff = 0, count = 0;
+
+	int** currArray = make2DIntArray(arrayX, arrayY);
+	int** nextArray = make2DIntArray(arrayX, arrayY);
+
+	for( i = 1; i < arrayX - 1; i++)
+	{
+		memcpy(nextArray[i], inArray[i], arrayX * sizeof(int));
+	}
+
+	while( diff == 0 )
+	{
+		count++;
+
+		//copy the next array into the working copy.
+		for( i = 1; i < arrayX - 1; i++)
+		{
+			memcpy(currArray[i], nextArray[i], arrayX * sizeof(int));
+		}
+
+		averageFour(currArray, nextArray, arrayX, arrayY);
+
+		diff = checkDiff(currArray, nextArray, arrayX, arrayY, precision);
+		if ((count % 1000) == 0)
+		{
+			printf("Looped %d times\n", count);
+		}
+	}
+
+	for( i = 1; i < arrayX - 1; i++)
+	{
+		memcpy(outArray[i], nextArray[i], arrayX * sizeof(int));
+	}
+
+}
+
+
+
 
 
 
 int main()
 {
 	int arraySize = 10;
-	int precision = 1;
+	int precision = 10;
+	int numThreads = 1;
 
 	printf("Creating 2 square arrays of arraySize %d\n", arraySize);
 
@@ -44,44 +106,11 @@ int main()
 	initArray(nextArray, arraySize);
 
 	printf("Initial array:\n");
-	printSquareArray(nextArray, arraySize);
+	printSquareArray(currArray, arraySize);
 
-	int i,j, diff = 0, count = 0;
+	threadAverage(currArray, nextArray, arraySize, arraySize, precision);
 
-	while( diff == 0 )
-	{
-		count++;
-
-		//copy the next array into the working copy.
-		for( i = 1; i < arraySize - 1; i++)
-		{
-			memcpy(currArray[i], nextArray[i], arraySize * sizeof(int));
-		}
-
-		//from pos 1 to arraySize-2 as edges are fixed
-		for(i = 1; i < arraySize - 1; i++)
-		{
-			for(j = 1; j < arraySize - 1; j++)
-			{
-				int n,s,e,w;
-				n	=	currArray[i-1][j];
-				s	=	currArray[i+1][j];
-				e	=	currArray[i][j+1];
-				w	=	currArray[i][j-1];
-
-				nextArray[i][j] = (n + s + e + w) / 4;
-
-			}
-		}
-
-		diff = checkDiff(currArray, nextArray, arraySize, precision);
-		if ((count % 1000) == 0)
-		{
-			printf("Looped %d times\n", count);
-		}
-	}
-
-	printf("Final output after %d loops:\n", count);
+	printf("Final output:\n");
 	printSquareArray(nextArray, arraySize);
 
 	free2DIntArray(currArray, arraySize);
